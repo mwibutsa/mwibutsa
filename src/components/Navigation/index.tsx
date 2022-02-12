0415279b35ac73f508111b683860a86ea287bfd5
Mwibutsa Floribert <mflohost@gmail.com>
2022-02-12 17:49:04 +0200

    Add other profile links

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,20 +1,16 @@
-import Button from "components/common/Button";
 import React, { ReactElement } from "react";
-import classNames from "./navigation.module.scss";
+import classes from "./navigation.module.scss";
 const Navigation: React.FC = (): ReactElement => {
   return (
-    <div className={classNames.Navigation}>
-      <h1 className={classNames.Logo}>Mwibutsa</h1>
-      <ul className={classNames.NavigationMenu}>
-        <li className={classNames.MenuItem}>About me</li>
-        <li className={classNames.MenuItem}>Experience</li>
-        <li className={classNames.MenuItem}>Project</li>
-        <li className={classNames.MenuItem}>Education</li>
-        <li className={classNames.MenuItem}>Let's talk</li>
+    <div className={classes.Navigation}>
+      <h1 className={classes.Logo}>Mwibutsa</h1>
+      <ul className={classes.NavigationMenu}>
+        <li className={classes.MenuItem}>About me</li>
+        <li className={classes.MenuItem}>Experience</li>
+        <li className={classes.MenuItem}>Project</li>
+        <li className={classes.MenuItem}>Education</li>
+        <li className={classes.MenuItem}>Let's talk</li>
       </ul>
-      <Button className="btn--primary" onClick={() => {}}>
-        <a href="mailto:floribert.mwibutsa@gmail.com">Contact</a>
-      </Button>
     </div>
   );
 };
